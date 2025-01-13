4e1f998bac46bbaf04bf04763b8d8321a2610fc5
MikolajSulkowski <mikolajsulkowski675@gmail.com>
2025-01-13 12:24:11 +0100

    update task

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ async def create_task(task: Task):
     for item in tasks.values():
         if item.title == task.title:
             raise HTTPException(detail=f"{item.title} already exists")
- 
+
     id = generate_id()
     tasks[id] = task
     return {"task_id": id, "task": task}
@@ -31,3 +31,22 @@ async def create_task(task: Task):
 @app.get("/tasks")
 async def get_tasks():
     return tasks
+
+@app.get("/tasks/{task_id}")
+async def get_task_info(id: str):
+    if id not in tasks.keys():
+        raise HTTPException(detail=f"{id} does not exist", status_code=404)
+    return tasks[id]
+
+
+@app.put("/tasks")
+async def update_task(id: str, task: Task):
+    if id not in tasks.keys():
+        raise HTTPException(detail=f"{id} does not exist")
+    for item in tasks.values():
+        if item.title == task.title:
+            raise HTTPException(detail=f"{item.title} already exists")
+    if task.status not in statuses:
+        raise HTTPException(detail=f"{task.status} does not exist")
+    tasks[id] = task
+    return {"task_id": id, "task": task}
