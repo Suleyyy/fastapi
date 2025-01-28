7c98f049077bbb2eecdc5277e2a251add387ae84
MikolajSulkowski <mikolajsulkowski675@gmail.com>
2025-01-28 15:56:22 +0100

    sql

--- main.py
+++ main.py
@@ -9,10 +9,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 if os.getenv("ENVIRONMENT") == "production":
-    SQLALCHEMY_DATABASE_URL = (
-        f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@"
-        f"{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
-    )
+    SQLALCHEMY_DATABASE_URL = (os.getenv("DATABASE_URL").replace("postgresql://", "postgresql+asyncpg://", 1) + "?sslmode=require")
 else:
     SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 
